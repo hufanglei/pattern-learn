6e61ff2f77916736ba26a62fc5bfdae41c85c0d0
hfl <690328661@qq.com>
2020-05-11 20:52:53 +0800

    懒汉模式之 内部类单例

---
+++ src/main/java/com/example/sington/lazy/LazyInnerClassSington.java
@@ -0,0 +1,25 @@
+package com.example.sington.lazy;
+
+/**
+ * Title: LazyInnerClassSington
+ * Description: 内部类单例
+ * 全程没有用到 synctronize关键字
+ * 性能最优的一种写法
+ * @author hfl
+ * @version V1.0
+ * @date 2020-05-11
+ */
+public class LazyInnerClassSington {
+    private LazyInnerClassSington() {
+    }
+    //懒汉式单例
+    //LazyHolder里面的逻辑需要等到外部方法调用时才执行
+    //巧妙利用了内部类的特性
+    //JVM底层执行逻辑，完美的避免了线程安全问题
+    public static final LazyInnerClassSington getInstance(){
+        return LazyHolder.lazy;
+    }
+    private static class LazyHolder{
+        private static final LazyInnerClassSington lazy = new LazyInnerClassSington();
+    }
+}
