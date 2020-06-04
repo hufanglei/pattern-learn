947c4b4de3062f9b9b88a4ef4dffbe267c3ffc0b
hfl <690328661@qq.com>
2020-06-05 06:44:30 +0800

    观察者模式

---
+++ src/main/java/com/example/observer/guava/GuavaEventTest.java
@@ -0,0 +1,28 @@
+package com.example.observer.guava;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * Created by Tom on 2019/3/17.
+ */
+public class GuavaEventTest {
+
+    public static void main(String[] args) {
+        //消息总线
+        EventBus eventBus = new EventBus();
+        GuavaEvent guavaEvent = new GuavaEvent();
+        GuavaEvent2 guavaEven2t = new GuavaEvent2();
+        eventBus.register(guavaEvent);
+        eventBus.register(guavaEven2t);
+        eventBus.post("Tom");
+        eventBus.post("hfl");
+
+        //从Struts到SpringMVC的升级
+        //因为Struts面向的类，而SpringMVC面向的是方法
+
+        //前面两者面向的是类，Guava面向是方法
+
+        //能够轻松落地观察模式的一种解决方案
+        //MQ
+    }
+}
---
+++ src/main/java/com/example/observer/observer.md
@@ -0,0 +1,29 @@
+# 观察者模式
+
+## 本质和类图相关解释
+
+## 2种方式 推和拉
+
+## 入门小demo  放哨看nba 漫画 或者研磨设计模式的部分
+
+## 应用场景一: 水质监测站（变形）
+
+## 应用场景二: 咕泡生活圈(使用java默认实现的抽象观察者和抽象被观察者)
+
+## 不依赖抽象观察者，解耦观察者的方式
+
+### 应用场景四: nba的反射委托例子
+
+### 应用场景五: swing组件的例子
+
+### 应用场景六: spring中的listener
+
+### 应用场景七: springboot的eventBus，到方法级别
+
+> 后续可能会有加上mq的例子，还没深入学习，先这样吧。
+
+
+
+https://blog.csdn.net/gdutxiaoxu/article/details/51824769
+https://blog.csdn.net/fw19940314/article/details/100010397
+
